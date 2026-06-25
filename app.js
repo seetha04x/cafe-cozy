ffc4c405bb4898591e4199ba5e3c330f31d1f750
seetha04x <seetha04x@gmail.com>
2026-06-26 02:23:57 +0530

    Add cloudinary files

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ if (process.env.NODE_ENV!="production"){
 }
 const express=require("express");
 const app=express();
+app.use(express.static('public'));
 const mongoose=require("mongoose");
 const Cafe=require("./models/cafe.js");
+const cafeRouter=require("./routes/cafe.js");
+const workRouter=require("./routes/workspace.js")
+const path=require("path");
+const ejsMate=require("ejs-mate");
+const methodOverride=require("method-override");
+const multer=require("multer");
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", 'ejs');
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"))
 
 async function main(){
-    console.log(process.env.ATLASDB_URL);
     await mongoose.connect(process.env.ATLASDB_URL);
 }
 main()
 .then(()=>{
     console.log("Connected to database CafeCozy.");
+    app.listen(2310,()=>{
+    console.log("Listening to port 2310...");
+})
 })
 .catch((err)=>{
     console.log("Error: ",err);
 })
 
-app.get("/cafes",async (req,res)=>{
-    const all= await Cafe.find({});
-    if(all.length ==0) {
-        return res.send("No cafes available right now");}
-    res.send("Hug&Mug,BobaLive,BunBoy");
-})
-app.listen(2310,()=>{
-    console.log("Listening to port 2310...");
-})
+app.use("/cafes",cafeRouter);
+app.use("/workspaces",workRouter);
