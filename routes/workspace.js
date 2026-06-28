cb37d008c705fefab3fdbeefec6b224a8d1a4a1b
seetha04x <seetha04x@gmail.com>
2026-06-28 23:48:02 +0530

    Add utils

--- routes/workspace.js
+++ routes/workspace.js
@@ -3,15 +3,16 @@ const Workspace=require("../models/workspace.js")
 const express=require("express");
 const router=express.Router();
 const workController=require("../controllers/workspace.js");
+const {isLoggedin, isOwner, validateListing}=require("../middleware.js");
 
 router.route("/")
-.post(workController.newPost);
-router.get("/:id/edit",workController.editForm);
+.post(isLoggedin, wrapAsync(workController.newPost));
+router.get("/:id/edit",isLoggedin, wrapAsync(workController.editForm));
 router.route("/:id")
-.get(workController.show)
-.delete(workController.delete)
-.put(workController.editPost);
+.get(wrapAsync(workController.show))
+.delete(isLoggedin, wrapAsync(workController.delete))
+.put(isLoggedin, wrapAsync(workController.editPost));
 
 
-router.get("/new/:id",workController.newForm);
+router.get("/new/:id",isLoggedin,workController.newForm);
 module.exports=router;
