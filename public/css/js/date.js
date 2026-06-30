4df51c5d91fb588dd8f7e901f883e75e00379679
seetha04x <seetha04x@gmail.com>
2026-07-01 02:21:46 +0530

    Add slots for booking

---
+++ public/css/js/date.js
@@ -0,0 +1,15 @@
+
+//   const dateInput = document.getElementById("date");
+//   const today = new Date().toISOString().split("T")[0];
+  
+//   // Show today as default
+//   dateInput.value = today;
+
+
+//   // Restore today if left empty
+//   dateInput.addEventListener("blur", () => {
+//     if (!dateInput.value) {
+//       dateInput.value = today;
+//     }
+//   });
+
