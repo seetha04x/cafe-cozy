d496003a231307290030c0157f40a0082d773f97
seetha04x <seetha04x@gmail.com>
2026-07-01 02:23:31 +0530

    Update router

--- routes/workspace.js
+++ routes/workspace.js
@@ -3,16 +3,24 @@ const Workspace=require("../models/workspace.js")
 const express=require("express");
 const router=express.Router();
 const workController=require("../controllers/workspace.js");
-const {isLoggedin, isOwner, validateListing}=require("../middleware.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const {isLoggedin, isOwner, validateWork}=require("../middlewares.js");
 
 router.route("/")
-.post(isLoggedin, wrapAsync(workController.newPost));
+.post(isLoggedin, validateWork, wrapAsync(workController.newPost));
+
+router.get("/booking",isLoggedin,wrapAsync(workController.bookingAll));
+
 router.get("/:id/edit",isLoggedin, wrapAsync(workController.editForm));
 router.route("/:id")
 .get(wrapAsync(workController.show))
 .delete(isLoggedin, wrapAsync(workController.delete))
-.put(isLoggedin, wrapAsync(workController.editPost));
+.put(isLoggedin, validateWork,wrapAsync(workController.editPost));
 
+router.route("/:id/booking")
+    .get(isLoggedin, wrapAsync(workController.bookingForm))
+    .post(isLoggedin, wrapAsync(workController.booking));
 
 router.get("/new/:id",isLoggedin,workController.newForm);
+
 module.exports=router;
