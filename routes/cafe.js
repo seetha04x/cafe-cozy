86d8d2b414d907b2dde3be472dce0b15e79ff04d
seetha04x <seetha04x@gmail.com>
2026-07-01 02:23:20 +0530

    Update router

--- routes/cafe.js
+++ routes/cafe.js
@@ -3,22 +3,25 @@ const express=require("express");
 const router=express.Router();
 const cafeController=require("../controllers/cafe.js");
 const multer=require("multer");
-const {storage}=require("../cloudConfig.js")
-const upload=multer({storage});
+const {storage}=require("../cloudConfig.js");
+
+const upload = storage
+    ? multer({ storage })
+    : multer({ storage: multer.memoryStorage() });
 
 const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
-const {isLoggedin, isOwner, validateListing}=require("../middleware.js");
+const {isLoggedin, isOwner, validateCafe}=require("../middlewares.js");
 
 router.route("/")
     .get( wrapAsync(cafeController.index))//display all cafes
-    .post(isLoggedin, upload.array("image"),wrapAsync(cafeController.postNew));
-
+    .post(isLoggedin, upload.array("image"),validateCafe,wrapAsync(cafeController.postNew));
+router.get("/search", wrapAsync(cafeController.search));
 router.get("/new",isLoggedin, cafeController.new); 
 router.route("/:id")
     .get(wrapAsync(cafeController.show))//to display individual listing
     .delete(isLoggedin, isOwner, wrapAsync(cafeController.delete))//to delete individual listing
-    .put(isLoggedin, isOwner, upload.array("image"), wrapAsync(cafeController.postEdit))
+    .put(isLoggedin, isOwner, upload.array("image"), validateCafe,wrapAsync(cafeController.postEdit))
 
 router.get("/:id/edit",isLoggedin, isOwner,         wrapAsync(cafeController.editForm));//to edit individual listing    
 module.exports=router;
