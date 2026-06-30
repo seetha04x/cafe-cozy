ab03d8f11f15f981f922aff90ae34eb80dc50702
seetha04x <seetha04x@gmail.com>
2026-07-01 02:26:36 +0530

    Update

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,18 +1,26 @@
-const cloudinary=require("cloudinary").v2;
-const { CloudinaryStorage}=require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET,
-})
+const cloudName = process.env.CLOUD_NAME;
+const cloudApiKey = process.env.CLOUD_API_KEY;
+const cloudApiSecret = process.env.CLOUD_API_SECRET;
 
-const storage= new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:{
-        folder:"CafeCozy",
-        allowed_formats:["png","jpg","jpeg"],
-    },
-})
+if (cloudName && cloudApiKey && cloudApiSecret) {
+    cloudinary.config({
+        cloud_name: cloudName,
+        api_key: cloudApiKey,
+        api_secret: cloudApiSecret,
+    });
+}
 
-module.exports={cloudinary, storage};
+const storage = cloudName && cloudApiKey && cloudApiSecret
+    ? new CloudinaryStorage({
+        cloudinary,
+        params: {
+            folder: "CafeCozy",
+            allowed_formats: ["png", "jpg", "jpeg"],
+        },
+    })
+    : undefined;
+
+module.exports = { cloudinary, storage };
